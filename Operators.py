95e98f1c01279c7c03ad4ae33fe7e9d7af0e4b56
mohammedkharoda <kharodawalam@gmail.com>
2022-06-05 18:03:37 +0530

    Basic Opreator

--- Operators.py
+++ Operators.py
@@ -1,1 +1,51 @@
-print('Hello World')
+# Additional Operators
+value1 = 2
+value2 = 3
+res = value1+value2
+print('Addition: ',res)
+
+# Subtraction Operations
+val1 = 2
+val2 = 3
+res = val1 + val2
+print('subtraction: ',res)
+
+# Multiplication
+val1 = 5
+val2 = 6
+resM = val1*val2
+print(resM)
+
+# Division Operator
+val1 = 10;
+val2 = 25;
+res = val1 + val2
+print(res)
+
+# Modulus Operator
+val1 = 3
+val2 = 4
+res=val1%val2
+print(res)
+
+# Exponention Operator
+val1 = 10
+val2 = 20
+res = val1 ** val2
+print(res)
+
+# Floor Division
+val1 = 25
+val2 = 96
+res = val1 // val2
+print(res)
+
+#
+#! Operator	Description	Syntax
+#? (+)	Addition: adds two operands	x + y
+#? (-)	Subtraction: subtracts two operands	x - y
+#? (*)	Multiplication: multiplies two operands	x * y
+#? (/)	Division (float): divides the first operand by the second	x / y
+#? (//)	Division (floor): divides the first operand by the second	x // y
+#? (%)	Modulus: returns the remainder when first operand is divided by the second	x % y
+#? (**)	Power : Returns first raised to power second	x ** y
