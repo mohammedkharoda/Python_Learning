f9b2b087738594a0619641766fc7739136fdd33d
mohammedkharoda <kharodawalam@gmail.com>
2022-06-06 12:46:58 +0530

    Operator Updates

--- Operators.py
+++ Operators.py
@@ -1,44 +1,44 @@
-# # Additional Operators
-# value1 = 2
-# value2 = 3
-# res = value1+value2
-# print('Addition: ',res)
-#
-# # Subtraction Operations
-# val1 = 2
-# val2 = 3
-# res = val1 + val2
-# print('subtraction: ',res)
-#
-# # Multiplication
-# val1 = 5
-# val2 = 6
-# resM = val1*val2
-# print(resM)
-#
-# # Division Operator
-# val1 = 10;
-# val2 = 25;
-# res = val1 + val2
-# print(res)
-#
-# # Modulus Operator
-# val1 = 3
-# val2 = 4
-# res=val1%val2
-# print(res)
-#
-# # Exponention Operator
-# val1 = 10
-# val2 = 20
-# res = val1 ** val2
-# print(res)
-#
-# # Floor Division
-# val1 = 25
-# val2 = 96
-# res = val1 // val2
-# print(res)
+# Additional Operators
+value1 = 2
+value2 = 3
+res = value1+value2
+print('Addition: ',res)
+
+# Subtraction Operations
+val1 = 2
+val2 = 3
+res = val1 + val2
+print('subtraction: ',res)
+
+# Multiplication
+val1 = 5
+val2 = 6
+resM = val1*val2
+print(resM)
+
+# Division Operator
+val1 = 10;
+val2 = 25;
+res = val1 + val2
+print(res)
+
+# Modulus Operator
+val1 = 3
+val2 = 4
+res=val1%val2
+print(res)
+
+# Exponention Operator
+val1 = 10
+val2 = 20
+res = val1 ** val2
+print(res)
+
+# Floor Division
+val1 = 25
+val2 = 96
+res = val1 // val2
+print(res)
 
 #
 #! Operator	Description	Syntax
