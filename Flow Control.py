b40a4538dbe9d1eac109a2817eb4c207dc6a85c1
mohammedkharoda <kharodawalam@gmail.com>
2022-06-07 10:45:56 +0530

    new day new file

---
+++ Flow Control.py
@@ -0,0 +1,6 @@
+#! ----------------- IF SATMENT --------------------------
+#? if statement is the most simple decision-making statement. It is used to decide whether a certain statement or block of statements will be executed or not i.e if a certain condition is true then a block of statement is executed otherwise not
+
+a = 10
+if(a>5):
+    print('The Value is greater than the 5: ', a)
