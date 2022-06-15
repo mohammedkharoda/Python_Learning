b04662c664b275e21984510773667a0e14532f61
mohammedkharoda <kharodawalam@gmail.com>
2022-06-15 14:51:31 +0530

    String Start

---
+++ Strings/String in Py.py
@@ -0,0 +1,14 @@
+#! ------------------------ Update of String -------------
+String1 = 'geeksForgeeks'
+print('Intial String: ')
+print(String1)
+
+String1 = 'Geek is Here'
+print('Update String: ')
+print(String1)
+
+# ! --------------------- deletion of string -------------
+del String1[2] #? -> Throw errors
+del String1
+print('Deleted String')
+print(String1) #! => Will throw an Error
